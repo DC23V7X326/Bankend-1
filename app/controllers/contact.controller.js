c6fb45251eb9c9826b5f0f24e5d79c8650c6b746
DC23V7X326 <vietdc23v7x326@dttx.ctu.edu.vn>
2025-04-22 23:27:32 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,27 +1,74 @@
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.util");
+const ApiError = require("../api-error");
+
 exports.create = (req, res) => {
     res.send({ message: "create handler"});
 };
+exports.create = async (req, res, next) => {
+    if (!req.body?.name) {
+        return next(new ApiError(400, "Tên không được để trống"));
+    }
 
-exports.findAll = (req, res) => {
-  res.send({ message: "findAll handler" });
-};
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.create(req.body);
+        return res.send(document);
+    } catch (error) {
+        return next(
+            new ApiError(500, "An error occurred while creating the contact")
+        );
+    }
+  };
+exports.findAll = async (req, res, next) => {
+    let document = [];
 
-exports.findOne = (req, res) => {
-  res.send({ message: "findOne handler" });
-};
+    try {
+        const contactServiceInstance = new ContactService(MongoDB.client);
+        const { name } = req.query;
 
-exports.update = (req, res) => {
-  res.send({ message: "update handler" });
-};
+        if (name) {
+            document = await contactServiceInstance.findByName(name);
+        } else {
+            document = await contactServiceInstance.find({});
+        }
+    } catch (error) {
+        return next(
+            new ApiError(500, "An error occurred while retrieving contacts")
+        );
+    }
+    return res.send(document);
+  };
+exports.findOne = async (req, res, next) => {
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.findById(req.params.id);
 
-exports.delete = (req, res) => {
-  res.send({ message: "delete handler" });
-};
+        if (!document) {
+            return next(new ApiError(404, "Contact not found"));
+        }
 
-exports.deleteAll = (req, res) => {
-  res.send({ message: "deleteAll handler" });
-};
-
-exports.findAllFavorite = (req, res) => {
-  res.send({ message: "findAllFavorite handler" });
+        return res.send(document);
+    } catch (error) {
+        return next(
+            new ApiError(500, `Error retrieving contact with id = ${req.params.id}`)
+        );
+    }
+};   
+exports.update = async (req, res, next) => {
+if (Object.keys(req.body).length === 0) {
+  return next(new ApiError(400, "Data to update can not be empty"));
+}
+try {
+  const contactService = new ContactService(MongoDB.client);
+  const document = await contactService.update(req.params.id, req.body);
+  if (!document){
+    return next(new ApiError(404, "Contact not found"));
+}
+  return res.send({message: "Contact was updated successfully"});
+} catch (error) {
+  return next(
+    new ApiError(500, 'Error updating  contact with id=${req.params.id}')
+  );
+}
 };
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-// Start server
-const PORT = config.app.port || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+async function startServer() {
+  try {
+    await MongoDB.connect(config.db.uri);
+    console.log("Connected to the database!");
+
+    const PORT = config.app.port;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("Cannot connect to the database!", error.message);
+    process.exit(1);
+  }
+}
